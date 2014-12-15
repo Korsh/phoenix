99f3c1f2badb94b997679338f17f2975de465483
Korsh <arzhan87@gmail.com>
2014-12-15 15:16:48 +0200

    Delete %%71^71F^71F0510D%%loading.tpl.php

--- templates/templates_c/%%71^71F^71F0510D%%loading.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.19, created on 2014-11-19 09:10:25
-         compiled from loading.tpl */ ?>
-<div id="loading">
-    <img src="/img/loading_icons/loading3.gif" height="500px">
-</div>
-<div id="informer">
-</div>
