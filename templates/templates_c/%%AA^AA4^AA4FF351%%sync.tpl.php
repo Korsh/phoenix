c8839e2549e12a1fa42a32484cdd77cdf51214be
Korsh <arzhan87@gmail.com>
2014-12-15 15:16:44 +0200

    Delete %%AA^AA4^AA4FF351%%sync.tpl.php

--- templates/templates_c/%%AA^AA4^AA4FF351%%sync.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.19, created on 2014-11-19 09:10:25
-         compiled from sync.tpl */ ?>
-<div id="syncForm">
-Synchronize <span id="syncAll">user</span>:<br>
-<input type="text" id="sync" size="70" title="Insert data for synchronize with 'Live' base, then click 'Sync'">
-<input type="button" onClick="sync($('#sync').val());" value="Sync" title="Insert data for synchronize with 'Live' base, then click 'Sync'">
-</div>
